e5909e0ab92cdcbd16f9a5e5dfaeb39f513237d6
Andrey M0shbear <andrey@moshbear.net>
2011-12-29 18:45:32 -0500

    block: add missing #include <cstddef>

--- include/mosh/fcgi/bits/block.hpp
+++ include/mosh/fcgi/bits/block.hpp
@@ -1,4 +1,4 @@
-//! @file mosh/fcgi/bits/block.hpp  Defines the (transceiver's) Block class
+//! @file  mosh/fcgi/bits/block.hpp  Defines the (transceiver's) Block class
 /***************************************************************************
 * Copyright (C) 2011 m0shbear                                              *
 *               2007 Eddie                                                 *
@@ -6,16 +6,16 @@
 * This file is part of mosh-fcgi.                                          *
 *                                                                          *
 * mosh-fcgi is free software: you can redistribute it and/or modify it     *
-* under the terms of the GnU Lesser General Public License as  published   *
+* under the terms of the GNU Lesser General Public License as  published   *
 * by the Free Software Foundation, either version 3 of the License, or (at *
 * your option) any later version.                                          *
 *                                                                          *
-* mosh-fcgi is distributed in the hope that it will be useful, but WITHOuT *
-* ANY WaRRANtY; without even the implied warranty of MERCHaNTABILItY or    *
-* FITNEsS FoR A PaRTICULAR PURPOsE.  See the GnU Lesser General Public     *
+* mosh-fcgi is distributed in the hope that it will be useful, but WITHOUT *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or    *
+* FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public     *
 * License for more details.                                                *
 *                                                                          *
-* You should have received a copy of the GnU Lesser General Public License *
+* You should have received a copy of the GNU Lesser General Public License *
 * along with mosh-fcgi.  If not, see <http://www.gnu.org/licenses/>.       *
 ****************************************************************************/
 
@@ -23,6 +23,7 @@
 #ifndef MOSH_FCGI_BLOCK_HPP
 #define MOSH_FCGI_BLOCK_HPP
 
+#include <cstddef>
 #include <mosh/fcgi/bits/namespace.hpp>
 
 MOSH_FCGI_BEGIN
