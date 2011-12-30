0d92db08cb2ba74deea87bec70c2aca143cc1e61
Andrey M0shbear <andrey@moshbear.net>
2011-12-30 00:37:10 -0500

    mk-headerlist.sh: show number of header files processed

--- include/mk-headerlist.sh
+++ include/mk-headerlist.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-echo -n 'Generating include/headerlist...'
-echo '#Automatically generated list of header files by mk-headerlist.sh, v0' > headerlist
+echo -n 'Generating include/headerlist ... '
+echo '#Automatically generated list of header files by mk-headerlist.sh, v1' > headerlist
 echo 'HEADER_LIST = \' >> headerlist
 find mosh/fcgi -type f -name '*.hpp' -o -name '*.tcc' | sed '$!s/$/ \\/' >> headerlist
 echo >> headerlist
-echo 'done'
+echo -n '('$(find mosh/fcgi -type f -name '*.hpp' -o -name '*.tcc' | wc -l)')'
+echo ' done'
 
