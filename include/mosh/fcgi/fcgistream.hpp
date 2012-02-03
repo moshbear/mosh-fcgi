57286b6da8a860ec551035cbc09986688f7e63df
Andrey m0shbear <andrey@moshbear.net>
2012-02-02 23:10:21 -0500

    Fcgistream refactoring.
    Fcgistream has been refactored from being a subclass of
    std::basic_ostream<T> to std::basic_ostream<unsigned char>.
    Now, we can buffer ASCII and binary data unless dump() is explicitly
    called.
    To maintain support for Unicode printing,
    operator << (Fcgistream&, std::wstring const&) internally converts to
    UTF-8 and uses iostreams buffering mechanisms for actual printing.
    
    This has the added benefit that byte streams are buffered as-is.
    
    The rationale for this refactor was primarily the fact that both Unicode
    and byte strings could not be buffered simultaneously, and the fact that
    unifying Fcgistream, as it were, makes iconv magic particularly
    troublesome.
    
    Since pretty much every browser supports UTF-8 and ASCII, pervasive use
    of iconv is unneccessary.
    
    Utf-8 support will be re-introduced in a later commit.

--- include/mosh/fcgi/fcgistream.hpp
+++ include/mosh/fcgi/fcgistream.hpp
@@ -1,17 +1,17 @@
 //! @file  mosh/fcgi/fcgistream.hpp Defines the Fcgistream stream and stream buffer classes
 /***************************************************************************
-* Copyright (C) 2011 m0shbear                                              *
+* Copyright (C) 2011-2 m0shbear                                              *
 *               2007 Eddie                                                 *
 *                                                                          *
 * This file is part of mosh-fcgi.                                          *
 *                                                                          *
 * mosh-fcgi is free software: you can redistribute it and/or modify it     *
-* under the terms of the Gn_u Lesser General Public License as  published   *
+* under the terms of the GNU Lesser General Public License as  published   *
 * by the Free Software Foundation, either version 3 of the License, or (at *
 * your option) any later version.                                          *
 *                                                                          *
 * mosh-fcgi is distributed in the hope that it will be useful, but WITHOUT *
-* ANY Wa_rRANTY; without even the implied warranty of MERCHANTABILITY or    *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or    *
 * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public     *
 * License for more details.                                                *
 *                                                                          *
@@ -22,7 +22,220 @@
 #ifndef MOSH_FCGI_FCGISTREAM_HPP
 #define MOSH_FCGI_FCGISTREAM_HPP
 
-#include <mosh/fcgi/fcgistream/fcgistream.hpp>
-#include <mosh/fcgi/fcgistream/fcgistream.tcc>
+#include <string>
+#include <streambuf>
+#include <ostream>
+#include <cstring>
+#include <algorithm>
+#include <ios>
+#include <istream>
+#include <locale>
+#include <memory>
+
+#include <mosh/fcgi/protocol/types.hpp>
+#include <mosh/fcgi/protocol/full_id.hpp>
+#include <mosh/fcgi/transceiver.hpp>
+#include <mosh/fcgi/bits/types.hpp>
+#include <mosh/fcgi/bits/namespace.hpp>
+
+MOSH_FCGI_BEGIN
+
+//! Stream class for output of client data through FastCGI
+/*!
+ * This class is derived from std::basic_ostream<unsigned char>. It acts just
+ * the same as any stream does with the added feature of the dump() function.
+ *
+ */
+class Fcgistream: public std::basic_ostream<uchar> {
+public:
+	Fcgistream() : std::basic_ostream<uchar>(&buffer) { }
+	//! Arguments passed directly to Fcgibuf::set()
+	void set(protocol::Full_id id, Transceiver& transceiver, protocol::Record_type type) {
+		buffer.set(id, transceiver, type);
+	}
+	/*! @name Dumpers
+	 */
+	//@{
+	/*! @brief Dumps raw data directly into the FastCGI protocol
+	 *
+	 * This function exists as a mechanism to dump raw data out the stream bypassing
+	 * the stream buffer. 
+	 *
+	 * @param[in] data Pointer to first byte of data to send
+	 * @param[in] size Size in bytes of data to be sent
+	 */
+	void dump(const uchar* data, size_t size) {
+		buffer.dump(data, size);
+	}
+	/*! @brief Dumps raw data directly into the FastCGI protocol
+	 *
+	 * This function exists as a mechanism to dump raw data out the stream bypassing
+	 * the stream buffer.
+	 *
+	 * @param[in] data Pointer to first byte of data to send
+	 * @param[in] size Size in bytes of data to be sent
+	 */
+	void dump(const char* data, size_t size) {
+		buffer.dump(reinterpret_cast<const uchar*>(data), size);
+	}
+	/*! @brief Dumps a byte string directy into the FastCGI protocol
+	 *
+	 * This function exists as a mechanism to dump raw data, in the form of a
+	 * %string out the stream bypassing the stream buffer.
+	 * 
+	 * @param[in] str Const-reference to string to print
+	 */
+	void dump(u_string const& str) {
+		buffer.dump(str.data(), str.size());
+	}
+	/*! @brief Dumps a byte string directy into the FastCGI protocol
+	 *
+	 * This function exists as a mechanism to dump raw data, in the form of a
+	 * %string out the stream bypassing the stream buffer.
+	 * 
+	 * @param[in] str Const-reference to string to print
+	 */
+	void dump(std::string const& str) {
+		buffer.dump(reinterpret_cast<const uchar*>(str.data()), str.size());
+	}
+	/*! @brief Dumps an input stream directly into the FastCGI protocol
+	 *
+	 * This function exists as a mechanism to dump a raw input stream out this stream bypassing
+	 * the stream buffer. Typically this would be a filestream
+	 * associated with an image or something. The stream is transmitted until an EOF.
+	 *
+	 * @param[in] stream Reference to input stream that should be transmitted.
+	 */
+	void dump(std::basic_istream<char>& stream);
+	//@}
+	/*! @brief Dumps an input stream directly into the FastCGI protocol
+	 *
+	 * This function exists as a mechanism to dump a raw input stream out this stream bypassing
+	 * the stream buffer. Typically this would be a filestream
+	 * associated with an image or something. The stream is transmitted until an EOF.
+	 *
+	 * @param[in] stream Reference to input stream that should be transmitted.
+	 */
+	void dump(std::basic_istream<uchar>& stream);
+	//@}
+
+private:
+	/*! @brief Stream buffer class for output of client data through FastCGI
+	 * This class is derived from std::basic_streambuf<_char_type, traits>. It acts just
+	 * the same as any stream buffer does with the added feature of the dump() function.
+	 */
+	class Fcgibuf: public std::basic_streambuf<uchar>
+	{
+	public:
+		Fcgibuf() : dump_ptr(0), dump_size(0) {
+			setp(buffer, buffer + buff_size);
+		}
+		/*! @brief After construction constructor
+		 * Sets FastCGI related member data necessary for operation of the
+		 * stream buffer.
+		 *
+		 * @param[in] id Complete ID associated with the request
+		 * @param[in] transceiver Transceiver object to use for transmission
+		 * @param[in] type Type of output stream (err or out)
+		 */
+		void set(protocol::Full_id id, Transceiver& transceiver, protocol::Record_type type) {
+			this->id = id;
+			this->transceiver = &transceiver;
+			this->type = type;
+		}
+
+		virtual ~Fcgibuf() {
+			try {
+				sync();
+			} catch (...) { } }
+		/*! @brief Dumps raw data directly into the FastCGI protocol
+		 * This function exists as a mechanism to dump raw data out the stream bypassing
+		 * the stream buffer or any code conversion mechanisms. If the user has any binary
+		 * data to send, this is the function to do it with.
+		 *
+		 * @param[in] data Pointer to first byte of data to send
+		 * @param[in] size Size in bytes of data to be sent
+		 */
+		void dump(const uchar* data, size_t size) {
+			dump_ptr = data;
+			dump_size = size;
+			sync();
+		}
+
+	private:
+		typedef typename std::basic_streambuf<uchar>::int_type int_type;
+		typedef typename std::basic_streambuf<uchar>::traits_type traits_type;
+		typedef typename std::basic_streambuf<uchar>::char_type char_type;
+		
+		int_type overflow(int_type c = traits_type::eof()) {
+			if (empty_buffer() < 0)
+				return traits_type::eof();
+			if (!traits_type::eq_int_type(c, traits_type::eof()))
+				return sputc(c);
+			else
+				return traits_type::not_eof(c);
+		}
+
+		int sync() {
+			return empty_buffer();
+		}
+
+		std::streamsize xsputn(const uchar* s, std::streamsize n);
+
+		//! Pointer to the data that needs to be transmitted upon flush
+		const uchar* dump_ptr;
+		//! Size of the data pointed to be dump_ptr
+		size_t dump_size;
+
+		//! Code converts, packages and transmits all data in the stream buffer along with the dump data
+		int empty_buffer();
+
+		//! Transceiver object to use for transmission
+		Transceiver* transceiver;
+		//! Complete ID associated with the request
+		protocol::Full_id id;
+		//! Type of output stream (err or out)
+		protocol::Record_type type;
+		//! Size of the internal stream buffer
+		static const int buff_size = 8192;
+		//! The buffer
+		uchar buffer[buff_size];
+	};
+	//! Stream buffer object
+	Fcgibuf buffer;
+	
+};
+
+/*! @brief Print a string to a Fcgistream
+ *  @param os The stream to print to
+ *  @param[in] s The string to print
+ *  @return os
+ */
+Fcgistream& operator << (Fcgistream& os, std::string const& s) {
+	os.write(reinterpret_cast<const unsigned char *>(s.data()), s.size());
+	return os;
+}
+
+/*! @brief Print a string to a Fcgistream
+ *
+ *  This function converts the wide string to a UTF-8 byte stream, then prints
+ *  it.
+ *  @param os The stream to print to
+ *  @param[in] ws The string to print
+ *  @return os
+ */
+Fcgistream& operator << (Fcgistream& os, std::wstring const& ws);
+
+/*! @brief Print a string to a Fcgistream
+ *  @param os The stream to print to
+ *  @param[in] s The string to print
+ *  @return os
+ */
+Fcgistream& operator << (Fcgistream& os, std::basic_string<uchar> const& us) {
+	os.write(us.data(), us.size());
+	return os;
+}
+
+MOSH_FCGI_END
 
 #endif
--- src/fcgistream.cpp
+++ src/fcgistream.cpp
@@ -1,6 +1,6 @@
-//! @file mosh/fcgi/fcgistream/fcgistream.tcc Defines member functions for Fcgistream
+//! @file fcgistream.cpp Defines member functions for Fcgistream
 /***************************************************************************
-* Copyright (C) 2011 m0shbear                                              *
+* Copyright (C) 2011-2 m0shbear                                            *
 *               2007 Eddie                                                 *
 *                                                                          *
 * This file is part of mosh-fcgi.                                          *
@@ -19,39 +19,26 @@
 * along with mosh-fcgi.  If not, see <http://www.gnu.org/licenses/>.       *
 ****************************************************************************/
 
-#ifndef MOSH_FCGI_FCGISTREAM_FCGISTREAM_TCC
-#define MOSH_FCGI_FCGISTREAM_FCGISTREAM_TCC
-
+#include <array>
 #include <algorithm>
 #include <limits>
 #include <locale>
 #include <memory>
-#include <stdexcept>
 #include <cstdint>
 #include <cstring>
 #include <ios>
-#include <mosh/fcgi/exceptions.hpp>
 #include <mosh/fcgi/bits/block.hpp>
-#ifndef MOSH_FCGI_USE_CGI
 #include <mosh/fcgi/protocol/types.hpp>
 #include <mosh/fcgi/protocol/full_id.hpp>
 #include <mosh/fcgi/protocol/header.hpp>
 #include <mosh/fcgi/protocol/vars.hpp>
-#else
-#include <mosh/fcgi/bits/array_deleter.hpp>
-extern "C" {
-#include <unistd.h>
-}
-#endif
 #include <mosh/fcgi/fcgistream.hpp>
-#include <mosh/fcgi/bits/iconv.hpp>
-#include <mosh/fcgi/bits/iconv_cvt.hpp>
+#include <mosh/fcgi/bits/utf8.hpp>
 #include <mosh/fcgi/bits/namespace.hpp>
 
 MOSH_FCGI_BEGIN
 
-template <class char_type, class traits>
-int Fcgistream<char_type, traits>::Fcgibuf::empty_buffer() {
+int Fcgistream::Fcgibuf::empty_buffer() {
 	using namespace std;
 	using namespace protocol;
 	char_type const* p_stream_pos = this->pbase();
@@ -60,54 +47,25 @@ int Fcgistream<char_type, traits>::Fcgibuf::empty_buffer() {
 		size_t wanted_size = count * sizeof(char_type) + dump_size;
 		if (!wanted_size)
 			break;
-#ifndef MOSH_FCGI_USE_CGI
 		int remainder = wanted_size % chunk_size;
 		wanted_size += sizeof(Header) + (remainder ? (chunk_size - remainder) : remainder);
-		if (wanted_size > numeric_limits<uint16_t>::max()) wanted_size = numeric_limits<uint16_t>::max();
+		if (wanted_size > numeric_limits<uint16_t>::max())
+			wanted_size = numeric_limits<uint16_t>::max();
+
 		Block data_block(transceiver->request_write(wanted_size));
 		data_block.size = (data_block.size / chunk_size) * chunk_size;
-#else
-		std::unique_ptr<char, Array_deleter<char>> data_buf(new char[wanted_size]);
-		Block data_block(data_buf.get(), wanted_size);
-#endif
 		locale loc = this->getloc();
-#ifndef MOSH_FCGI_USE_CGI
 		char* to_next = data_block.data + sizeof(Header);
-#else
-		char* to_next = data_block.data;
-#endif
 		if (count) {
-			if (sizeof(char_type) != sizeof(char)) {
-				Iconv::IC_state* i = ic.get();
-				if (use_facet<codecvt<char_type, char, Iconv::IC_state*> >(loc)
-				        .out(i, p_stream_pos, this->pptr(), p_stream_pos,
-				             to_next, data_block.data + data_block.size, to_next)
-				        == codecvt_base::error) {
-					pbump(-(this->pptr() - this->pbase()));
-					dump_size = 0;
-					dump_ptr = 0;
-#ifndef MOSH_FCGI_USE_CGI
-					throw exceptions::Stream(id);
-#else
-					throw std::runtime_error(std::string("stream"));
-#endif
-				}
-			} else {
-#ifndef MOSH_FCGI_USE_CGI
-				size_t cnt = min(data_block.size - sizeof(Header), count);
-				memcpy(data_block.data + sizeof(Header), p_stream_pos, cnt);
-#else
-				size_t cnt = min(data_block.size, count);
-#endif
-				p_stream_pos += cnt;
-				to_next += cnt;
-			}
+			size_t cnt = min(data_block.size - sizeof(Header), count);
+			memcpy(data_block.data + sizeof(Header), p_stream_pos, cnt);
+			p_stream_pos += cnt;
+			to_next += cnt;
 		}
 		size_t dumped_size = min(dump_size, static_cast<size_t>(data_block.data + data_block.size - to_next));
 		memcpy(to_next, dump_ptr, dumped_size);
 		dump_ptr += dumped_size;
 		dump_size -= dumped_size;
-#ifndef MOSH_FCGI_USE_CGI
 		uint16_t content_length = to_next - data_block.data + dumped_size - sizeof(Header);
 		uint8_t content_remainder = content_length % chunk_size;
 		Header& header = *(Header*)data_block.data;
@@ -117,22 +75,17 @@ int Fcgistream<char_type, traits>::Fcgibuf::empty_buffer() {
 		header.content_length() = content_length;
 		header.padding_length() = content_remainder ? (chunk_size - content_remainder) : content_remainder;
 		transceiver->secure_write(sizeof(Header) + content_length + header.padding_length(), id, false);
-#else
-		write(fd, data_block.data, data_block.size);
-#endif
-		
 	}
 	pbump(-(this->pptr() - this->pbase()));
 	return 0;
 }
 
-template <class _char_type, class traits>
-std::streamsize Fcgistream<_char_type, traits>::Fcgibuf::xsputn(const _char_type *s, std::streamsize n)
+std::streamsize Fcgistream::Fcgibuf::xsputn(const uchar* s, std::streamsize n)
 {
 	std::streamsize remainder = n;
 	while (remainder) {
 		std::streamsize actual = std::min(remainder, this->epptr() - this->pptr());
-		std::memcpy(this->pptr(), s, actual * sizeof(_char_type));
+		std::memcpy(this->pptr(), s, actual);
 		this->pbump(actual);
 		remainder -= actual;
 		if (remainder) {
@@ -144,17 +97,38 @@ std::streamsize Fcgistream<_char_type, traits>::Fcgibuf::xsputn(const _char_type
 	return n;
 }
 
-template<class char_type, class traits > void Fcgistream<char_type, traits>::dump(std::basic_istream<char>& stream)
+void Fcgistream::dump(std::basic_istream<char>& stream)
 {
-	const size_t buffer_size = 32768;
-	char buffer[buffer_size];
+	std::array<char, 32768> buffer;
 
 	while (stream.good()) {
-		stream.read(buffer, buffer_size);
+		stream.read(buffer, buffer.size());
 		dump(buffer, stream.gcount());
 	}
 }
 
+void Fcgistream::dump(std::basic_istream<uchar>& stream)
+{
+	std::array<uchar, 32768> buffer;
+
+	while (stream.good()) {
+		stream.read(buffer, buffer.size());
+		dump(buffer, stream.gcount());
+	}
+}
+
+Fcgistream& operator << (Fcgistream& os, std::wstring const& ws) {
+	const wchar_t* w_begin = ws.begin();
+	const wchar_t* w_end = ws.end();
+	std::array<uchar, 32768> buffer;
+	char *s_next;
+	while (w_begin != w_end) {
+		if (utf8_out(w_begin, w_end, w_begin, buffer, buffer + 32768, s_next) < 0)
+			throw std::invalid_argument("Fcgistream::operator<<(Fcgistream&, std::wstring const&): EILSEQ");
+		os.write(buffer, std::distance(buffer, s_next);
+	}
+	return os;
+}
+
 MOSH_FCGI_END
 
-#endif
