1fd18696e04c85e922ba5017cb513834a6487e1f
Andrey m0shbear <andrey@moshbear.net>
2012-02-25 08:39:06 -0500

    transceiver: move almost everything to src/
    With the bulk of transceiver in src/, the include'd file now uses the
    pimpl idiom to save on compile the time.

--- include/mosh/fcgi/transceiver.hpp
+++ include/mosh/fcgi/transceiver.hpp
@@ -1,48 +1,37 @@
 //! @file  mosh/fcgi/transceiver.hpp Defines the MOSH_FCGI::Transceiver class
 /***************************************************************************
-* Copyright (C) 2011 m0shbear                                              *
+* Copyright (C) 2011-2 m0shbear                                            *
 *               2007 Eddie                                                 *
 *                                                                          *
 * This file is part of mosh-fcgi.                                          *
 *                                                                          *
 * mosh-fcgi is free software: you can redistribute it and/or modify it     *
-* under the terms of the GnU Lesser General Public License as  published   *
+* under the terms of the GNU Lesser General Public License as  published   *
 * by the Free Software Foundation, either version 3 of the License, or (at *
 * your option) any later version.                                          *
 *                                                                          *
-* mosh-fcgi is distributed in the hope that it will be useful, but WITHOuT *
-* ANY WaRRANtY; without even the implied warranty of MERCHaNTABILItY or    *
-* FITNEsS FoR A PaRTICULAR PURPOsE.  See the GnU Lesser General Public     *
+* mosh-fcgi is distributed in the hope that it will be useful, but WITHOUT *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or    *
+* FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public     *
 * License for more details.                                                *
 *                                                                          *
-* You should have received a copy of the GnU Lesser General Public License *
+* You should have received a copy of the GNU Lesser General Public License *
 * along with mosh-fcgi.  If not, see <http://www.gnu.org/licenses/>.       *
 ****************************************************************************/
-
-#ifdef MOSH_FCGI_USE_CGI
-#error "MOSH_FCGI_USE_CGI must be undefined in FastCGI (default) mode"
-#endif
 
 
 #ifndef MOSH_FCGI_TRANSCEIVER_HPP
 #define MOSH_FCGI_TRANSCEIVER_HPP
 
 #include <map>
-#include <list>
-#include <queue>
-#include <algorithm>
-#include <map>
-#include <vector>
 #include <memory>
-
+#include <vector>
 extern "C" {
-#include <unistd.h>
 #include <poll.h>
-#include <sys/socket.h>
 }
 
-#include <mosh/fcgi/bits/array_deleter.hpp>
 #include <mosh/fcgi/bits/block.hpp>
+#include <mosh/fcgi/bits/types.hpp>
 #include <mosh/fcgi/protocol/header.hpp>
 #include <mosh/fcgi/protocol/message.hpp>
 #include <mosh/fcgi/exceptions.hpp>
@@ -54,8 +43,7 @@ MOSH_FCGI_BEGIN
  * This class handles the sending/receiving/buffering of data through the OS level sockets and also
  * the creation/destruction of the sockets themselves.
  */
-class Transceiver
-{
+class Transceiver {
 public:
 	//! General transceiver handler
 	/*!
@@ -67,15 +55,10 @@ public:
 	 */
 	bool handler();
 
-	//! Direct interface to Buffer::request_write()
-	Block request_write(size_t size) {
-		return buffer.request_write(size);
-	}
-	//! Direct interface to Buffer::secure_write()
-	void secure_write(size_t size, protocol::Full_id id, bool kill) {
-		buffer.secure_write(size, id, kill);
-		transmit();
-	}
+	//! Interface to Buffer::request_write()
+	Block request_write(size_t size);
+	//! Interface to Buffer::secure_write()
+	void secure_write(size_t size, protocol::Full_id id, bool kill);
 	//! Constructor
 	/*!
 	 * Construct a transceiver object based on an initial file descriptor to listen on and
@@ -86,16 +69,10 @@ public:
 	 */
 	Transceiver(int fd, std::function<void(protocol::Full_id, protocol::Message)> send_message);
 	//! Blocks until there is data to receive or a call to wake() is made
-	void sleep() {
-		poll(&poll_fds.front(), poll_fds.size(), -1);
-	}
+	void sleep();
 
 	//! Forces a wakeup from a call to sleep()
-	void wake() {
-		char x;
-		ssize_t _unused = write(wakeup_fd_out, &x, 1);
-		_unused = _unused;
-	}
+	void wake();
 
 private:
 	//! %Buffer type for receiving FastCGI records
@@ -107,136 +84,11 @@ private:
 	};
 
 	//! %Buffer type for transmission of FastCGI records
-	/*!
-	 * This buffer is implemented as a circle of Chunk objects; the number of which can grow and shrink as needed. Write
-	 * space is requested with request_write() which thereby returns a Block which may be smaller
-	 * than requested. The write is committed by calling secure_write(). A smaller space can be
-	 * committed than was given to write on.
-	 *
-	 * All data written to the buffer has an associated file descriptor through which it
-	 * is flushed. File descriptor association with data is managed through a queue of Frame
-	 * objects.
-	 */
-	class Buffer
-	{
-		//! %Frame of data associated with a file descriptor
-		struct Frame {
-			//! Constructor
-			/*!
-			 * @param[in] size Size of the frame
-			 * @param[in] close_fd Boolean value indication whether or not the file descriptor should be closed when the frame has been flushed
-			 * @param[in] id Complete ID of the request making the frame
-			 */
-			Frame(size_t size, bool close_fd, protocol::Full_id id)
-				: size(size), close_fd(close_fd), id(id)
-			{ }
-			//! Size of the frame
-			size_t size;
-			//! Boolean value indication whether or not the file descriptor should be closed when the frame has been flushed
-			bool close_fd;
-			//! Complete ID (contains a file descriptor) of associated with the data frame
-			protocol::Full_id id;
-		};
-		//! Queue of frames waiting to be transmitted
-		std::queue<Frame> frames;
-		//! Minimum Block size value that can be returned from request_write()
-		const static unsigned int min_block_size = 256;
-		//! A reference to Transceiver::poll_fds for removing file descriptors when they are closed
-		std::vector<pollfd>& poll_fds;
-		//! A reference to Transceiver::Fd_buffer for deleting buffers upon closing of the file descriptor
-		std::map<int, Fd_buffer>& fd_buffers;
-		//! %Chunk of data in Buffer
-		struct Chunk {
-			//! Size of data section of the chunk
-			const static unsigned int size = 131072;
-			//! Pointer to the first byte in the chunk data
-			std::shared_ptr<char> data;
-			//! Pointer to the first write byte in the chunk or 1+ the last read byte
-			char* end;
-			//! Creates a new data chunk
-			Chunk(): data(new char[size], Array_deleter<char>()), end(data.get()) { }
-			~Chunk() { }
-			//! Creates a new object that shares the data of the old one
-			Chunk(const Chunk& chunk): data(chunk.data), end(data.get()) { }
-		};
-
-		//! A list of chunks. Can contain from 2-infinity
-		std::list<Chunk> chunks;
-		//! Iterator pointing to the chunk currently used for writing
-		std::list<Chunk>::iterator write_it;
-
-		//! Current read spot in the buffer
-		char* p_read;
-
-	public:
-		//! Constructor
-		/*!
-		 * @param[out] poll_fds A reference to Transceiver::poll_fds is needed for removing file descriptors when they are closed
-		 * @param[out] fd_buffers A reference to Transceiver::Fd_buffer is needed for deleting buffers upon closing of the file descriptor
-		 */
-		Buffer(std::vector<pollfd>& poll_fds, std::map<int, Fd_buffer>& fd_buffers)
-			: poll_fds(poll_fds), fd_buffers(fd_buffers), chunks(1), write_it(chunks.begin()), p_read(chunks.begin()->data.get())
-		{ }
-
-		//! Request a write block in the buffer
-		/*!
-		 * @param[in] size Requested size of write block
-		 * @return Block of writable memory. Size may be less than requested
-		 */
-		Block request_write(size_t size) {
-			return Block(write_it->end, std::min(size, (size_t)(write_it->data.get() + Chunk::size - write_it->end)));
-		}
-		//! Secure a write in the buffer
-		/*!
-		 * @param[in] size Amount of bytes to secure
-		 * @param[in] id Associated complete ID (contains file descriptor)
-		 * @param[in] kill Boolean value indicating whether or not the file descriptor should be closed after transmission
-		 */
-		void secure_write(size_t size, protocol::Full_id id, bool kill);
-
-		//! %Block of memory for extraction from Buffer
-		struct Send_block {
-			//! Constructor
-			/*!
-			 * @param[in] data Pointer to the first byte in the block
-			 * @param[in] size Size in bytes of the data
-			 * @param[in] fd File descriptor the data should be written to
-			 */
-			Send_block(const char* data, size_t size, int fd): data(data), size(size), fd(fd) { }
-			//! Create a new object that shares the data of the old
-			Send_block(const Send_block& send_block): data(send_block.data), size(send_block.size), fd(send_block.fd) { }
-			//! Pointer to the first byte in the block
-			const char* data;
-			//! Size in bytes of the data
-			size_t size;
-			//! File descriptor the data should be written to
-			int fd;
-		};
-
-		//! Request a block of data for transmitting
-		/*!
-		 * @return A block of data with a file descriptor to transmit it out
-		 */
-		Send_block request_read() {
-			return Send_block(p_read, frames.empty() ? 0 : frames.front().size, frames.empty() ? -1 : frames.front().id.fd);
-		}
-		//! Mark data in the buffer as transmitted and free it's memory
-		/*!
-		 * @param size Amount of bytes to mark as transmitted and free
-		 */
-		void free_read(size_t size);
-
-		//! Test of the buffer is empty
-		/*!
-		 * @return true if the buffer is empty
-		 */
-		bool empty() {
-			return p_read == write_it->end;
-		}
-	};
+	class Buffer;
 
 	//! %Buffer for transmitting data
-	Buffer buffer;
+	std::unique_ptr<Buffer> pbuf;
+	
 	//! Function to call to pass messages to requests
 	std::function<void(protocol::Full_id, protocol::Message)> send_message;
 
--- src/transceiver.cpp
+++ src/transceiver.cpp
@@ -19,9 +19,14 @@
 * along with mosh-fcgi.  If not, see <http://www.gnu.org/licenses/>.       *
 ****************************************************************************/
 
+#include <algorithm>
 #include <functional>
-#include <vector>
 #include <limits>
+#include <list>
+#include <map>
+#include <memory>
+#include <queue>
+#include <vector>
 
 extern "C" {
 #include <unistd.h>
@@ -43,6 +48,11 @@ extern "C" {
 #include <mosh/fcgi/transceiver.hpp>
 #include <mosh/fcgi/bits/namespace.hpp>
 
+#include <src/array_deleter.hpp>
+#include <src/namespace.hpp>
+
+namespace {
+
 //! Predicate for comparing the file descriptor of a pollfd
 struct equals_fd : public std::unary_function<pollfd, bool> {
 	int fd;
@@ -52,11 +62,153 @@ struct equals_fd : public std::unary_function<pollfd, bool> {
 	};
 };
 
+}
+
 MOSH_FCGI_BEGIN
+
+//! %Buffer type for transmission of FastCGI records
+/*!
+ * This buffer is implemented as a circle of Chunk objects; the number of which can grow and shrink as needed. Write
+ * space is requested with request_write() which thereby returns a Block which may be smaller
+ * than requested. The write is committed by calling secure_write(). A smaller space can be
+ * committed than was given to write on.
+ *
+ * All data written to the buffer has an associated file descriptor through which it
+ * is flushed. File descriptor association with data is managed through a queue of Frame
+ * objects.
+ */
+class Transceiver::Buffer {
+	//! %Frame of data associated with a file descriptor
+	struct Frame {
+		//! Constructor
+		/*!
+		 * @param[in] size Size of the frame
+		 * @param[in] close_fd Boolean value indication whether or not the file descriptor should be closed when the frame has been flushed
+		 * @param[in] id Complete ID of the request making the frame
+		 */
+		Frame(size_t size, bool close_fd, protocol::Full_id id)
+			: size(size), close_fd(close_fd), id(id)
+		{ }
+		//! Size of the frame
+		size_t size;
+		//! Boolean value indication whether or not the file descriptor should be closed when the frame has been flushed
+		bool close_fd;
+		//! Complete ID (contains a file descriptor) of associated with the data frame
+		protocol::Full_id id;
+	};
+	//! Queue of frames waiting to be transmitted
+	std::queue<Frame> frames;
+	//! Minimum Block size value that can be returned from request_write()
+	const static unsigned int min_block_size = 256;
+	//! A reference to Transceiver::poll_fds for removing file descriptors when they are closed
+	std::vector<pollfd>& poll_fds;
+	//! A reference to Transceiver::Fd_buffer for deleting buffers upon closing of the file descriptor
+	std::map<int, Fd_buffer>& fd_buffers;
+	//! %Chunk of data in Buffer
+	struct Chunk {
+		//! Size of data section of the chunk
+		const static unsigned int size = 131072;
+		//! Pointer to the first byte in the chunk data
+		std::shared_ptr<uchar> data;
+		//! Pointer to the first write byte in the chunk or 1+ the last read byte
+		uchar* end;
+		//! Creates a new data chunk
+		Chunk(): data(new uchar[size], SRC::Array_deleter<uchar>()), end(data.get()) { }
+		~Chunk() { }
+		//! Creates a new object that shares the data of the old one
+		Chunk(const Chunk& chunk): data(chunk.data), end(data.get()) { }
+	};
+	//! A list of chunks. Can contain from 2-infinity
+	std::list<Chunk> chunks;
+	//! Iterator pointing to the chunk currently used for writing
+	std::list<Chunk>::iterator write_it;
+	//! Current read spot in the buffer
+	uchar* p_read;
+public:
+	//! Constructor
+	/*!
+	 * @param[out] poll_fds A reference to Transceiver::poll_fds is needed for removing file descriptors when they are closed
+	 * @param[out] fd_buffers A reference to Transceiver::Fd_buffer is needed for deleting buffers upon closing of the file descriptor
+	 */
+	Buffer(std::vector<pollfd>& poll_fds, std::map<int, Fd_buffer>& fd_buffers)
+		: poll_fds(poll_fds), fd_buffers(fd_buffers), chunks(1), write_it(chunks.begin()), p_read(chunks.begin()->data.get())
+	{ }
+	//! Request a write block in the buffer
+	/*!
+	 * @param[in] size Requested size of write block
+	 * @return Block of writable memory. Size may be less than requested
+	 */
+	Block request_write(size_t size) {
+		return Block(write_it->end, std::min(size, (size_t)(write_it->data.get() + Chunk::size - write_it->end)));
+	}
+	//! Secure a write in the buffer
+	/*!
+	 * @param[in] size Amount of bytes to secure
+	 * @param[in] id Associated complete ID (contains file descriptor)
+	 * @param[in] kill Boolean value indicating whether or not the file descriptor should be closed after transmission
+	 */
+	void secure_write(size_t size, protocol::Full_id id, bool kill);
+		//! %Block of memory for extraction from Buffer
+	struct Send_block {
+		//! Constructor
+		/*!
+		 * @param[in] data Pointer to the first byte in the block
+		 * @param[in] size Size in bytes of the data
+		 * @param[in] fd File descriptor the data should be written to
+		 */
+		Send_block(const uchar* data, size_t size, int fd): data(data), size(size), fd(fd) { }
+		//! Create a new object that shares the data of the old
+		Send_block(const Send_block& send_block): data(send_block.data), size(send_block.size), fd(send_block.fd) { }
+		//! Pointer to the first byte in the block
+		const uchar* data;
+		//! Size in bytes of the data
+		size_t size;
+		//! File descriptor the data should be written to
+		int fd;
+	};
+	//! Request a block of data for transmitting
+	/*!
+	 * @return A block of data with a file descriptor to transmit it out
+	 */
+	Send_block request_read() {
+		return Send_block(p_read, frames.empty() ? 0 : frames.front().size, frames.empty() ? -1 : frames.front().id.fd);
+	}
+	//! Mark data in the buffer as transmitted and free it's memory
+	/*!
+	 * @param size Amount of bytes to mark as transmitted and free
+	 */
+	void free_read(size_t size);
+	//! Test of the buffer is empty
+	/*!
+	 * @return true if the buffer is empty
+	 */
+	bool empty() {
+		return p_read == write_it->end;
+	}
+};
+
+Block Transceiver::request_write(size_t size) {
+	return pbuf->request_write(size);
+}
+
+void Transceiver::secure_write(size_t size, protocol::Full_id id, bool kill) {
+	pbuf->secure_write(size, id, kill);
+	transmit();
+}
+
+void Transceiver::sleep() {
+	poll(&poll_fds.front(), poll_fds.size(), -1);
+}
+
+void Transceiver::wake() {
+	char x;
+	ssize_t _unused = write(wakeup_fd_out, &x, 1);
+	_unused = _unused;
+}
 
 int Transceiver::transmit() {
 	for(;;) {
-		Buffer::Send_block send_block(buffer.request_read());
+		Buffer::Send_block send_block(pbuf->request_read());
 		if (send_block.size) {
 			ssize_t sent = write(send_block.fd, send_block.data, send_block.size);
 			if (sent < 0) {
@@ -67,14 +219,14 @@ int Transceiver::transmit() {
 				} else if (errno != EAGAIN)
 					throw exceptions::Socket_write(send_block.fd, errno);
 			}
-			buffer.free_read(sent);
+			pbuf->free_read(sent);
 			assert (send_block.size <= std::numeric_limits<ssize_t>::max()); 
 			if (sent != send_block.size)
 				break;
 		} else
 			break;
 	}
-	return buffer.empty();
+	return pbuf->empty();
 }
 
 void Transceiver::Buffer::secure_write(size_t size, protocol::Full_id id, bool kill) {
@@ -136,7 +288,7 @@ bool Transceiver::handler() {
 	// Are we in the process of recieving some part of a frame?
 	if (!message_buffer.data) {
 		// Are we recieving a partial header or new?
-		actual = read(fd, reinterpret_cast<char*>(&header_buffer) + message_buffer.size,
+		actual = read(fd, reinterpret_cast<uchar*>(&header_buffer) + message_buffer.size,
 					sizeof(Header) - message_buffer.size);
 		if (actual < 0 && errno != EAGAIN)
 			throw exceptions::Socket_read(fd, errno);
@@ -146,7 +298,7 @@ bool Transceiver::handler() {
 			return (transmit_empty);
 		}
 
-		message_buffer.data.reset(new char[sizeof(Header)
+		message_buffer.data.reset(new uchar[sizeof(Header)
 						   + header_buffer.content_length()
 						   + header_buffer.padding_length()
 						  ]);
@@ -157,7 +309,7 @@ bool Transceiver::handler() {
 	Header& header = _header;
 	size_t needed = header.content_length() + header.padding_length() + sizeof(Header) - message_buffer.size;
 	assert(needed <= std::numeric_limits<ssize_t>::max()); // Send a bug report if this assertion fails
-	actual = read(fd, static_cast<char*>(message_buffer.data.get()) + message_buffer.size, needed);
+	actual = read(fd, static_cast<uchar*>(message_buffer.data.get()) + message_buffer.size, needed);
 	if (actual < 0 && errno != EAGAIN)
 		throw exceptions::Socket_read(fd, errno);
 	if (actual > 0)
@@ -200,7 +352,7 @@ void Transceiver::Buffer::free_read(size_t size) {
 }
 
 Transceiver::Transceiver(int fd_, std::function<void(protocol::Full_id, protocol::Message)> send_message_)
-	: buffer(poll_fds, fd_buffers), send_message(send_message_), poll_fds(2), socket(fd_)  {
+	: pbuf(new Buffer(poll_fds, fd_buffers)), send_message(send_message_), poll_fds(2), socket(fd_)  {
 	// Let's setup an in/out socket for waking up poll()
 	int soc_pair[2];
 	socketpair(AF_UNIX, SOCK_STREAM, 0, soc_pair);
