d050730924e639dc67509065d345ceac82d663b3
Andrey m0shbear <andrey@moshbear.net>
2012-02-25 15:29:39 -0500

    ci_strcomp: fixups
    - Change all the char operations to be pass-by-value over pass-by-cref
    	- negligible copy overhead
    - Typo fixes in the lambdas

--- include/mosh/fcgi/bits/ci_strcomp.hpp
+++ include/mosh/fcgi/bits/ci_strcomp.hpp
@@ -31,21 +31,21 @@
 
 MOSH_FCGI_BEGIN
 
-template <typename char_type> char_type _xlower(const char_type&);
-template<> char _xlower<char>(const char& ch) { return std::tolower(ch); }
-template<> uchar _xlower<uchar>(const uchar& ch) { return std::tolower(ch); }
-template<> wchar_t _xlower<wchar_t>(const wchar_t& wc) { return std::towlower(wc); }
+template <typename char_type> char_type _xlower(char_type);
+template<> char _xlower<char>(char ch) { return std::tolower(ch); }
+template<> uchar _xlower<uchar>(uchar ch) { return std::tolower(ch); }
+template<> wchar_t _xlower<wchar_t>(wchar_t wc) { return std::towlower(wc); }
 
 // Case-insensitive char comparator
 template <class charT>
-bool ci_charcmp(charT&& ch1, charT&& ch2, Cmp_test test) {
+bool ci_charcmp(charT ch1, charT ch2, Cmp_test test) {
 	switch (test) {
-	case Cmp_test::eq: return (_xlower(o1) == _xlower(o2));
-	case Cmp_test::ne: return (_xlower(o1) != _xlower(o2));
-	case Cmp_test::lt: return (_xlower(o1) < _xlower(o2));
-	case Cmp_test::le: return (_xlower(o1) <= _xlower(o2));
-	case Cmp_test::ge: return (_xlower(o1) >= _xlower(o2));
-	case Cmp_test::gt: return (_xlower(o1) > _xlower(o2));
+	case Cmp_test::eq: return (_xlower(ch1) == _xlower(ch2));
+	case Cmp_test::ne: return (_xlower(ch1) != _xlower(ch2));
+	case Cmp_test::lt: return (_xlower(ch1) < _xlower(ch2));
+	case Cmp_test::le: return (_xlower(ch1) <= _xlower(ch2));
+	case Cmp_test::ge: return (_xlower(ch1) >= _xlower(ch2));
+	case Cmp_test::gt: return (_xlower(ch1) > _xlower(ch2));
 	}
 	return false;
 }
@@ -61,11 +61,11 @@ bool ci_charcmp(charT&& ch1, charT&& ch2, Cmp_test test) {
  * @return @c true if the comparison succeeds
  */
 template <class charT>
-bool ci_cmp(std::basic_string<charT>&& s1, std::basic_string<charT>&& s2, Cmp_test op = Cmp_test::eq)
+bool ci_cmp(std::basic_string<charT> const& s1, std::basic_string<charT> const& s2, Cmp_test op = Cmp_test::eq)
 {
 	return range_cmp(s1, s2, op,
-				[] (charT&& ch1, charT&& ch2) {
-					return ci_charcmp<charT>(ch1, ch2, op);
+				[] (charT ch1, charT ch2, Cmp_test op_) {
+					return ci_charcmp<charT>(ch1, ch2, op_);
 				}
 			);
 }
@@ -81,18 +81,18 @@ bool ci_cmp(std::basic_string<charT>&& s1, std::basic_string<charT>&& s2, Cmp_te
  * @return @c true if the comparison succeeds
  */
 template <class charT>
-bool ci_cmp(std::basic_string<charT>&& ss1, std::basic_string<charT>&& ss2, size_t n, Cmp_test op = Cmp_test::eq)
+bool ci_cmp(std::basic_string<charT> const& ss1, std::basic_string<charT> const& ss2, size_t n, Cmp_test op = Cmp_test::eq)
 {
-	return range_cmp(s1, s2, n, op,
-				[] (charT&& ch1, charT&& ch2) {
-					return ci_charcmp<charT>(ch1, ch2, op);
+	return range_cmp(ss1, ss2, n, op,
+				[] (charT ch1, charT ch2, Cmp_test op_) {
+					return ci_charcmp<charT>(ch1, ch2, op_);
 				}
 			);
 }
 
 template <class charT, Cmp_test cmp_t = Cmp_test::eq>
 struct ci_cmp_wrapper {
-	bool operator()(const std::basic_string<charT>& s1, const std::basic_string<charT>& s2) const {
+	bool operator()(std::basic_string<charT> const& s1, std::basic_string<charT> const& s2) const {
 		return ci_cmp(s1, s2, cmp_t);
 	}
 };
