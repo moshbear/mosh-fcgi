8b85dd1dcfa905ad1eca52a66cb8e27f26876ea7
Andrey m0shbear <andrey@moshbear.net>
2012-04-03 18:00:57 -0400

    build.sh now stops if any stage exits with error

--- build.sh
+++ build.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
-echo '**mk-headerslist**'
-cd include
-./mk-headerlist.sh
-cd ..
-echo '**autogen**'
-./autogen.sh
-echo '**configure**'
-./configure CXX="ccache g++ -std=c++0x"
-echo '**make**'
-make
+echo '**mk-headerslist**' && \
+cd include && \
+./mk-headerlist.sh && \
+cd .. && \
+echo '**autogen**' && \
+./autogen.sh && \
+echo '**configure**' && \
+./configure CXX="ccache g++ -std=c++0x" && \
+echo '**make clean**' && \
+make clean && \
+echo '**make**' && \
+make && \
+echo '**done**'
